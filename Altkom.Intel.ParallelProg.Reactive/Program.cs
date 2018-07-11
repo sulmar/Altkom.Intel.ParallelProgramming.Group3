fe1d5c0472cbcdc36d0353fa16b6ed2e725a4c8d
Marcin Sulecki <marcin.sulecki@altkom.pl>
2018-07-11 15:01:37 +0200

    FileSystemWatcherTest

--- Altkom.Intel.ParallelProg.Reactive/Program.cs
+++ Altkom.Intel.ParallelProg.Reactive/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Altkom.Intel.ParallelProg.Reactive
@@ -36,9 +40,131 @@ namespace Altkom.Intel.ParallelProg.Reactive
 
         static void Main(string[] args)
         {
+            FileSystemWatcherTest();
+
+            // CpuMonitorTest();
+
+
+            // RandomSourceTest();
 
 
             // gorące źródło
+            // HotSourceTest();
+
+            // zimne źródło
+            //  ReplaySubjectTest();
+
+
+            // ColdSourceTest();
+
+            //SendSMS("Hello World");
+            //SendPost("Hello World");
+
+            //DelegatesTest();
+
+            //EventsTest();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        private static void FileSystemWatcherTest()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            var watcher = new FileSystemWatcher(path);
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;  // (!!!)
+
+            // watcher.Created += Watcher_Created;
+            //watcher.Deleted += Watcher_Deleted;
+
+            var files = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>
+            (
+                h => { watcher.Created += h; watcher.Deleted += h; },
+                h => { watcher.Created -= h; watcher.Deleted -= h; }
+            )
+            .Select(p => p.EventArgs)
+            ;
+
+            files
+                .Where(file => Path.GetExtension(file.Name) == ".txt")
+                .Subscribe(file => Console.WriteLine($"{file.ChangeType} {file.Name}"));
+
+
+            //var created = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>
+            //(
+            //    h => watcher.Created += h,
+            //    h => watcher.Created -= h
+            //)
+            //.Select(p => p.EventArgs);
+
+            //created.Subscribe(file => Console.WriteLine($"Created {file.Name}"));
+
+            //var deleted = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>
+            //(
+            //    h => watcher.Deleted += h,
+            //    h => watcher.Deleted -= h
+            //)
+            //.Select(p => p.EventArgs);
+
+            //deleted.Subscribe(file =>
+            //{
+            //    Console.BackgroundColor = ConsoleColor.Red;
+            //    Console.WriteLine($"Deleted {file.Name}");
+            //    Console.ResetColor();
+            //}
+            //);
+        }
+
+        private static void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Deleted {e.Name}");
+            Console.ResetColor();
+        }
+
+        private static void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Created {e.Name}");
+        }
+
+        private static void CpuMonitorTest()
+        {
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            // gorące źródło
+            var source = Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .Select(_ => cpuCounter.NextValue());
+
+            source
+                .Subscribe(cpu => Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} [CPU] {cpu}"));
+
+            source
+                .Where(cpu => cpu > 70)
+                .Subscribe(cpu => Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} [ALERT] {cpu}"));
+        }
+
+        private static void RandomSourceTest()
+        {
+            Random random = new Random();
+
+            // gorące źródło
+            var source = Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .Select(_ => random.Next(0, 100));
+
+            source
+                .Subscribe(amount => Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} [Marcin] {amount}"));
+
+            source
+                .Where(amount => amount > 80)
+                .Subscribe(amount => Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} [Bartek] {amount}"));
+        }
+
+        private static void HotSourceTest()
+        {
             var source = new Subject<decimal>();
 
             MyObserver observer = new MyObserver("Marcin");
@@ -66,23 +192,6 @@ namespace Altkom.Intel.ParallelProg.Reactive
             source.OnCompleted();
 
             source.OnNext(99.99m);
-
-            // zimne źródło
-            //  ReplaySubjectTest();
-
-
-
-            // ColdSourceTest();
-
-            //SendSMS("Hello World");
-            //SendPost("Hello World");
-
-            //DelegatesTest();
-
-            //EventsTest();
-
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
         }
 
         private static void ReplaySubjectTest()
